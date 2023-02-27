d16b06b5d507bbbfe6401ada99b445c800a17fdf
Mirek Nguyen <nguyemir@fit.cvut.cz>
2023-02-27 13:13:28 +0100

    refactor: doc comments for options

--- lua/options.lua
+++ lua/options.lua
@@ -4,28 +4,25 @@ local options = {
   relativenumber = true, -- relative number lines
   ignorecase = true, -- search case insensitive
   smartcase = true, -- case sensitive when search query contains uppercase
-  tabstop = 2, -- number of visual spaces per TAB
-  shiftwidth = 2, -- number of spaces to use for autoindent
-  softtabstop = 2, -- numbers of spaces to use for autoindent
-  expandtab = true, -- tabs are spaces
   shiftround = true, -- round to nearest shiftwidth, applies to < > commands
-  smartindent = true,
-  smarttab = true,
-  autoindent = true,
+  shiftwidth = 2, -- number of spaces to use for autoindent
+  tabstop = 2, -- number of visual spaces per TAB
+  softtabstop = 2, -- number of visual spaces per new line
+  expandtab = true, -- tabs are spaces
+  smartindent = true, -- automatic indentation for new lines based on syntax
   copyindent = true, -- copy indent from the previous line
-  completeopt = { "menu", "menuone", "noselect" },
+  completeopt = { "menu", "menuone", "noselect" }, -- for completion plugin
   termguicolors = true,
-  swapfile = false,
-  compatible = false,
-  splitright = false,
-  laststatus = 0,
+  swapfile = false, -- store unsaved changes in a file
   shell = "/bin/bash", -- set shell
   updatetime = 50,
-  ruler = false,
-  showmode = false, -- insert, visual mode message on the last line
-  showcmd = false,
   undofile = true,
   wrap = false,
+  laststatus = 0,
+  ruler = false,
+  showmode = false, -- insert, visual mode message on the last line
+  showcmd = false, -- show last pressed keys in status line
+  fillchars = 'eob: ', -- disable '~' blank lines
 }
 vim.g.mapleader = " "
 
@@ -33,10 +30,7 @@ for key, value in pairs(options) do
   vim.opt[key] = value
 end
 
-vim.api.nvim_command("syntax on")
 vim.g.loaded_python3_provider = 0
 vim.g.loaded_ruby_provider = 0
 vim.g.loaded_node_provider = 0
 vim.g.loaded_perl_provider = 0
--- vim.wo.fillchars = 'eob: ' -- disable '~' blank lines
-vim.opt.fillchars:append('eob: ')
