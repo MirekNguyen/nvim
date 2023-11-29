14b5bdf18dc65e2d09ef6118186fc7b07807a5fc
Mirek Nguyen <mireknguyenbinh@seznam.cz>
2023-11-29 22:58:02 +0100

    feat: add yamlls, sqlls

--- lua/lsp-options.lua
+++ lua/lsp-options.lua
@@ -5,10 +5,14 @@ return {
     pyright = {},
     clangd = {},
     cssls = {},
-    emmet_ls = {},
+    emmet_ls = {
+      filetypes = { "astro", "css", "eruby", "html", "htmldjango", "javascriptreact", "less", "pug", "sass", "scss", "svelte", "typescriptreact", "vue", "twig" }
+    },
     tailwindcss = { filetypes = { "typescriptreact", "javascriptreact" } },
     tsserver = {},
-    html = {},
+    html = {
+      filetypes = { "html", "twig" }
+    },
     jsonls = {},
     intelephense = {
       init_options = { globalStoragePath = os.getenv('HOME') .. '/.local/share/intelephense' },
@@ -16,6 +20,8 @@ return {
     dockerls = {},
     lua_ls = {},
     rust_analyzer = {},
+    yamlls = {},
+    sqlls = {},
   },
   sources = function(null_ls)
     return {
@@ -24,15 +30,20 @@ return {
           return { "--tab-width=" .. params.options.tabSize }
         end,
       }),
+      -- php
       null_ls.builtins.diagnostics.phpcs.with({ extra_args = { "--standard=PSR12" } }),
-      null_ls.builtins.diagnostics.phpstan,
-      null_ls.builtins.formatting.phpcsfixer,
+      null_ls.builtins.formatting.phpcsfixer.with({ extra_args = { "--using-cache=no" } }),
+      null_ls.builtins.diagnostics.twigcs,
+      -- bash
       null_ls.builtins.code_actions.shellcheck,
       null_ls.builtins.diagnostics.shellcheck,
+      -- python
       null_ls.builtins.formatting.isort,
       null_ls.builtins.formatting.black,
-      null_ls.builtins.formatting.stylua,
+      -- rust
       null_ls.builtins.formatting.rustfmt,
+      -- lua
+      null_ls.builtins.formatting.stylua,
     }
   end
 }
