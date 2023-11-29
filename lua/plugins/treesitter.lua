d836754201f7925ee52c3bb777dd7b9c914bf724
Mirek Nguyen <mireknguyenbinh@seznam.cz>
2023-11-29 22:58:24 +0100

    feat: more treesitter

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -3,7 +3,7 @@ if (not status) then return end
 
 require 'nvim-treesitter.configs'.setup {
   ensure_installed = { "bash", "c", "c_sharp", "cpp", "css", "fish", "html", "java", "javascript", "json", "lua", "php",
-    "python", "scss", "tsx", "typescript", "vim", "yaml", "markdown", "markdown_inline", "latex" },
+    "python", "scss", "tsx", "typescript", "vim", "yaml", "markdown", "markdown_inline", "latex", "twig", "sql", "xml", "rust", "gitignore", "dockerfile" },
   highlight = {
     enable = true,
     disable = {},
