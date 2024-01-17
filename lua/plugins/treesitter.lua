81d9e04be29b02b1a62565dddeb80b8d2184c492
Mirek Nguyen <mireknguyenbinh@seznam.cz>
2024-01-17 01:10:07 +0100

    feat(treesitter): add syntax to common formats

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,18 +1,51 @@
-local status, _ = pcall(require, 'nvim-treesitter')
-if (not status) then return end
+local status, _ = pcall(require, "nvim-treesitter")
+if not status then
+	return
+end
 
-require 'nvim-treesitter.configs'.setup {
-  ensure_installed = { "bash", "c", "c_sharp", "cpp", "css", "fish", "html", "java", "javascript", "json", "lua", "php",
-    "python", "scss", "tsx", "typescript", "vim", "yaml", "markdown", "markdown_inline", "latex", "twig", "sql", "xml", "rust", "gitignore", "dockerfile" },
-  highlight = {
-    enable = true,
-    disable = {},
-  },
-  indent = {
-    enable = true,
-    disable = {},
-  },
-  autotag = {
-    enable = true,
-  }
-}
+require("nvim-treesitter.configs").setup({
+	ensure_installed = {
+		"bash",
+		"c",
+		"c_sharp",
+		"cpp",
+		"css",
+		"fish",
+		"html",
+		"java",
+		"javascript",
+		"json",
+		"lua",
+		"php",
+		"phpdoc",
+		"python",
+		"scss",
+		"tsx",
+		"typescript",
+		"vim",
+		"yaml",
+		"markdown",
+		"markdown_inline",
+		"latex",
+		"twig",
+		"sql",
+		"xml",
+		"rust",
+		"gitignore",
+		"dockerfile",
+		"svelte",
+		"toml",
+		"requirements",
+	},
+	highlight = {
+		enable = true,
+		disable = {},
+	},
+	indent = {
+		enable = true,
+		disable = {},
+	},
+	autotag = {
+		enable = true,
+	},
+})
