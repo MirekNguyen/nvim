f5b26197cd028c1cd00fb80acac1e66c1bd65e0d
Mirek Nguyen <mireknguyenbinh@seznam.cz>
2024-02-14 13:20:18 +0100

    feat: add phpactor

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -27,3 +27,21 @@ for server, args in pairs(servers) do
   config.single_file_support = (args.single_file_support or true)
   lspconfig[server].setup(config)
 end
+
+lspconfig.phpactor.setup({
+  autostart = false,
+  on_attach = function(client, bufnr)
+    client.server_capabilities.hoverProvider = false
+    client.server_capabilities.definitionProvider = false
+  end,
+  init_options = {
+    ["language_server_worse_reflection.inlay_hints.enable"] = true,
+    ["language_server_worse_reflection.inlay_hints.params"] = true,
+    ["language_server_worse_reflection.inlay_hints.types"] = true,
+    -- ["language_server_configuration.auto_config"] = false,
+    -- ["code_transform.import_globals"] = true,
+    -- ["language_server_phpstan.enabled"] = true,
+    -- ["language_server_phpstan.level"] = 7,
+    -- ["language_server_phpstan.bin"] = "phpstan",
+  },
+})
