9040911773d476168d4d249d5f534e50535e6507
Mirek Nguyen <mirek.nguyen@o2.cz>
2024-09-15 01:26:23 +0200

    feat: github workflow autocomplete

--- lua/config/lsp-options.lua
+++ lua/config/lsp-options.lua
@@ -94,6 +94,7 @@ return {
 					schemas = {
 						["https://gitlab.com/gitlab-org/gitlab/-/raw/master/app/assets/javascripts/editor/schema/ci.json"] = "*gitlab-ci*.{yml,yaml}",
 						["https://raw.githubusercontent.com/yannh/kubernetes-json-schema/master/master/all.json"] = "/*.k8s.{yaml,yml}",
+						["https://json.schemastore.org/github-workflow.json"] = ".github/workflows/*.{yml,yaml}",
 					},
 					format = {
 						enable = true,
