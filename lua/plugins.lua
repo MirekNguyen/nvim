403a642d09cbcb53e0846148ac80b8643f464309
Mirek Nguyen <mirek.nguyen@o2.cz>
2024-09-14 14:25:34 +0200

    chore: treesitter to separate file

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -11,129 +11,14 @@ if not vim.loop.fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 return require("lazy").setup({
-  {
-    "vhyrro/luarocks.nvim",
-    priority = 1000,
-    config = true,
-    opts = {
-      rocks = { "lua-curl", "nvim-nio", "mimetypes", "xml2lua" },
-    },
-    lazy = false,
-  },
-  -- {
-  --   "rest-nvim/rest.nvim",
-  --   ft = "http",
-  --   dependencies = { "luarocks.nvim" },
-  --   config = function()
-  --     require("rest-nvim").setup()
-  --   end,
-  --   lazy = false,
-  -- },
+  require("plugins.colorscheme"),
   { "sindrets/diffview.nvim", cmd = "DiffviewOpen" },
-  {
-    "jackMort/ChatGPT.nvim",
-    cmd = { "ChatGPT", "ChatGPTRun" },
-    config = function()
-      require("chatgpt").setup({
-        chat = {
-          openai_params = {
-            model = "gpt-4o-2024-08-06",
-            max_tokens = 4096,
-          },
-          openai_edit_params = {
-            model = "gpt-4o-2024-08-06",
-          },
-          keymaps = {
-            select_session = "o",
-          },
-        },
-      })
-    end,
-    dependencies = {
-      "MunifTanjim/nui.nvim",
-      "nvim-lua/plenary.nvim",
-      "nvim-telescope/telescope.nvim",
-    },
-  },
-  {
-    "tpope/vim-fugitive",
-    cmd = "Git",
-  },
-  {
-    "luckasRanarison/tailwind-tools.nvim",
-    config = true,
-    filetypes = { "html", "css", "scss", "javascript", "typescript", "javascriptreact", "typescriptreact" },
-  },
-  {
-    "piersolenski/wtf.nvim",
-    dependencies = {
-      "MunifTanjim/nui.nvim",
-    },
-    opts = {
-      openai_model_id = "gpt-4o-2024-08-06",
-    },
-    cmd = { "Wtf", "WtfSearch" },
-  },
-  {
-    "stevearc/oil.nvim",
-    cmd = "Oil",
-    opts = {
-      view_options = {
-        show_hidden = true,
-      }
-    }
-  },
-  {
-    "kristijanhusak/vim-dadbod-ui",
-    dependencies = {
-      { "tpope/vim-dadbod",                     lazy = true },
-      { "kristijanhusak/vim-dadbod-completion", ft = { "sql", "mysql", "plsql" }, lazy = true },
-    },
-    cmd = {
-      "DBUI",
-      "DBUIToggle",
-      "DBUIAddConnection",
-      "DBUIFindBuffer",
-    },
-    config = function()
-      vim.g.db_ui_show_help = 0
-      vim.api.nvim_create_autocmd({ "BufReadPost" }, {
-        pattern = { "*.dbout" },
-        callback = function()
-          vim.api.nvim_exec2(
-            [[
-        exe ':resize 40'
-        ]],
-            {}
-          )
-        end,
-      })
-      vim.api.nvim_create_autocmd({ "BufReadPost" }, {
-        pattern = { "*.dbout" },
-        callback = function()
-          vim.api.nvim_exec2(
-            [[
-        exe ':resize 20'
-        ]],
-            {}
-          )
-        end,
-      })
-    end,
-    init = function()
-      vim.g.db_ui_use_nerd_fonts = 1
-    end,
-  },
-  {
-    "ThePrimeagen/harpoon",
-    dependencies = { "nvim-lua/plenary.nvim" },
-    branch = "harpoon2",
-    event = "VeryLazy",
-    config = function()
-      local harpoon = require("harpoon")
-      harpoon:setup()
-    end,
-  },
+  require("plugins.chatgpt-nvim"),
+  require("plugins.tailwind-tools"),
+  require("plugins.oil"),
+  require("plugins.dadbod"),
+  require("plugins.wtf"),
+  require("plugins.harpoon"),
   {
     "ray-x/lsp_signature.nvim",
     event = "InsertEnter",
@@ -143,56 +28,13 @@ return require("lazy").setup({
     },
   },
   {
-    "ellisonleao/gruvbox.nvim",
-    config = function()
-      vim.cmd([[colorscheme gruvbox]])
-    end,
-    lazy = false,
-  },
-  {
     "mikesmithgh/kitty-scrollback.nvim",
     config = true,
     cmd = { "KittyScrollbackGenerateKittens", "KittyScrollbackCheckHealth" },
     event = { "User KittyScrollbackLaunch" },
   },
-  {
-    "nvim-telescope/telescope.nvim",
-    config = function()
-      require("telescope").setup({
-        extensions = {
-          fzf = {
-            fuzzy = true,             -- false will only do exact matching
-            override_generic_sorter = true, -- override the generic sorter
-            override_file_sorter = true, -- override the file sorter
-            case_mode = "smart_case", -- or "ignore_case" or "respect_case"
-          },
-          telescope_path = {
-            search_dir = "$HOME",
-            ignore_file = "$HOME/.config/dotfiles/config/fdignore",
-          },
-        },
-      })
-      require("telescope").load_extension("telescope_path")
-      require("telescope").load_extension("fzf")
-    end,
-    cmd = "Telescope",
-    dependencies = {
-      "nvim-lua/plenary.nvim",
-      "mireknguyen/telescope-path.nvim",
-      "nvim-tree/nvim-web-devicons",
-      { "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
-    },
-  },
-  {
-    "nvim-treesitter/nvim-treesitter",
-    event = { "BufReadPost", "BufNewFile" },
-    config = function()
-      require("plugins.treesitter")
-    end,
-    build = function()
-      require("nvim-treesitter.install").update({ with_sync = true })
-    end,
-  },
+  require("plugins.telescope"),
+  require("plugins.treesitter"),
   {
     "neovim/nvim-lspconfig",
     event = { "BufReadPost", "BufNewFile" },
@@ -235,54 +77,15 @@ return require("lazy").setup({
   },
   {
     "nvimtools/none-ls.nvim",
-    -- event = { "BufReadPost", "BufNewFile" },
-    lazy = false,
+    event = { "BufReadPost", "BufNewFile" },
+    -- lazy = false,
     config = function()
       require("plugins.null-ls")
     end,
     dependencies = { "nvim-lua/plenary.nvim" },
   },
-  {
-    "glepnir/lspsaga.nvim",
-    event = "LspAttach",
-    config = function()
-      require("lspsaga").setup({
-        symbol_in_winbar = {
-          enable = false,
-        },
-        lightbulb = {
-          sign = false,
-        },
-      })
-    end,
-  },
-  {
-    "zbirenbaum/copilot.lua",
-    cmd = "Copilot",
-    build = ":Copilot auth",
-    event = "InsertEnter",
-    opts = {
-      panel = {
-        enabled = true,
-        auto_refresh = true, -- auto refresh panel
-        layout = {
-          position = "right", -- | top | left | right
-          ratio = 0.4,   -- width of the panel, when position is left or right
-        },
-      },
-      suggestion = {
-        enabled = true,  -- enable suggestions
-        auto_trigger = true, -- automatically show suggestions
-        accept = false,  -- disable built-in keymapping
-        keymap = {
-          accept = "<C-f>",
-          next = "<C-;>",
-          prev = "<C-'>",
-          dismiss = "<C-\\>",
-        },
-      },
-    },
-  },
+  require("plugins.lsp-saga"),
+  require("plugins.copilot"),
 }, {
   performance = {
     rtp = {
@@ -293,11 +96,11 @@ return require("lazy").setup({
         -- "netrwPlugin",
         "tarPlugin", -- edit tar files
         "tohtml",
-        "tutor",
+        -- "tutor",
         "zipPlugin",
         -- "rplugin", -- remote plugins
         "spellfile",
-        "man", -- ':Man' command (man pages)
+        -- "man", -- ':Man' command (man pages)
         "shada", -- search, command history, marks
         "health", -- ':checkhealth' command
       },
--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,54 +1,58 @@
-local status, _ = pcall(require, "nvim-treesitter")
-if not status then
-	return
-end
+local tsconfig = {
+  ensure_installed = {
+    "bash",
+    "c",
+    "c_sharp",
+    "cpp",
+    "css",
+    "fish",
+    "html",
+    "java",
+    "javascript",
+    "json",
+    "lua",
+    "php",
+    "phpdoc",
+    "python",
+    "scss",
+    "tsx",
+    "typescript",
+    "vim",
+    "yaml",
+    "markdown",
+    "markdown_inline",
+    "latex",
+    "twig",
+    "sql",
+    "xml",
+    "rust",
+    "gitignore",
+    "dockerfile",
+    "svelte",
+    "toml",
+    "requirements",
+    "prisma",
+    "http",
+    "graphql",
+  },
+  highlight = {
+    enable = true,
+    disable = {},
+  },
+  indent = {
+    enable = true,
+    disable = {},
+  },
+  autotag = {
+    enable = true,
+  },
+}
 
-require("nvim-treesitter.configs").setup({
-	ensure_installed = {
-		"bash",
-		"c",
-		"c_sharp",
-		"cpp",
-		"css",
-		"fish",
-		"html",
-		"java",
-		"javascript",
-		"json",
-		"lua",
-		"php",
-		"phpdoc",
-		"python",
-		"scss",
-		"tsx",
-		"typescript",
-		"vim",
-		"yaml",
-		"markdown",
-		"markdown_inline",
-		"latex",
-		"twig",
-		"sql",
-		"xml",
-		"rust",
-		"gitignore",
-		"dockerfile",
-		"svelte",
-		"toml",
-		"requirements",
-		"prisma",
-		"http",
-		"graphql",
-	},
-	highlight = {
-		enable = true,
-		disable = {},
-	},
-	indent = {
-		enable = true,
-		disable = {},
-	},
-	autotag = {
-		enable = true,
-	},
-})
+return {
+  "nvim-treesitter/nvim-treesitter",
+  event = { "BufReadPost", "BufNewFile" },
+  build = ":TSUpdate",
+  config = function()
+    require("nvim-treesitter.configs").setup(tsconfig)
+  end,
+}
