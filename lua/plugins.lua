bf18bb58525f0c8ea8b1ad304d39a91c8c4894cf
Mirek Nguyen <mirek.nguyen@o2.cz>
2024-09-14 15:05:08 +0200

    refactor: move lspconfig to plugin file

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -12,48 +12,17 @@ end
 vim.opt.rtp:prepend(lazypath)
 return require("lazy").setup({
   require("plugins.colorscheme"),
-  { "sindrets/diffview.nvim", cmd = "DiffviewOpen" },
   require("plugins.chatgpt-nvim"),
   require("plugins.tailwind-tools"),
   require("plugins.oil"),
   require("plugins.dadbod"),
   require("plugins.wtf"),
   require("plugins.harpoon"),
-  {
-    "ray-x/lsp_signature.nvim",
-    event = "InsertEnter",
-    config = true,
-    opts = {
-      hint_enable = false,
-    },
-  },
-  {
-    "mikesmithgh/kitty-scrollback.nvim",
-    config = true,
-    cmd = { "KittyScrollbackGenerateKittens", "KittyScrollbackCheckHealth" },
-    event = { "User KittyScrollbackLaunch" },
-  },
+  require("plugins.lsp-signature"),
+  require("plugins.kitty-scrollback"),
   require("plugins.telescope"),
   require("plugins.treesitter"),
-  {
-    "neovim/nvim-lspconfig",
-    event = { "BufReadPost", "BufNewFile" },
-    cmd = { "Mason", "LspInstall", "LspUninstall", "LspInfo" },
-    config = function()
-      require("plugins.lsp")
-    end,
-    dependencies = {
-      {
-        "williamboman/mason-lspconfig.nvim",
-        config = true,
-        opts = { automatic_installation = true },
-        dependencies = {
-          "williamboman/mason.nvim",
-          config = true,
-        },
-      },
-    },
-  },
+  require('plugins.lsp'),
   {
     "hrsh7th/nvim-cmp",
     event = "InsertEnter",
@@ -67,7 +36,7 @@ return require("lazy").setup({
           require("plugins.luasnip")
         end,
       },
-      { "onsails/lspkind.nvim" },
+      "onsails/lspkind.nvim",
       "hrsh7th/cmp-buffer",
       "hrsh7th/cmp-path",
       "hrsh7th/cmp-cmdline",
@@ -75,15 +44,7 @@ return require("lazy").setup({
       "saadparwaiz1/cmp_luasnip",
     },
   },
-  {
-    "nvimtools/none-ls.nvim",
-    event = { "BufReadPost", "BufNewFile" },
-    -- lazy = false,
-    config = function()
-      require("plugins.null-ls")
-    end,
-    dependencies = { "nvim-lua/plenary.nvim" },
-  },
+  require('plugins.none-ls'),
   require("plugins.lsp-saga"),
   require("plugins.copilot"),
 }, {
@@ -96,11 +57,11 @@ return require("lazy").setup({
         -- "netrwPlugin",
         "tarPlugin", -- edit tar files
         "tohtml",
-        -- "tutor",
+        "tutor",
         "zipPlugin",
         -- "rplugin", -- remote plugins
         "spellfile",
-        -- "man", -- ':Man' command (man pages)
+        "man", -- ':Man' command (man pages)
         "shada", -- search, command history, marks
         "health", -- ':checkhealth' command
       },
--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,60 +1,78 @@
-local status, lspconfig = pcall(require, 'lspconfig')
-if (not status) then return end
+local function configuration()
+  local lspconfig = require('lspconfig')
+  local capabilities = vim.lsp.protocol.make_client_capabilities()
+  capabilities.textDocument.completion.completionItem.snippetSupport = true
 
-local capabilities = vim.lsp.protocol.make_client_capabilities()
-capabilities.textDocument.completion.completionItem.snippetSupport = true
+  local signs = { Error = "", Warn = "", Hint = "", Info = "" }
+  for type, icon in pairs(signs) do
+    local hl = "DiagnosticSign" .. type
+    vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
+  end
 
-local signs = { Error = "", Warn = "", Hint = "", Info = "" }
-for type, icon in pairs(signs) do
-  local hl = "DiagnosticSign" .. type
-  vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
+  -- Check if lsp-options-custom.lua exists
+  local user_lsp_config_path = vim.fn.stdpath('config') .. '/lua/lsp-options-custom.lua'
+  local lsp_options
+
+  if vim.fn.filereadable(user_lsp_config_path) == 1 then
+    lsp_options = require 'lsp-options-custom'
+  else
+    lsp_options = require 'lsp-options'
+  end
+  local servers = lsp_options.servers
+
+  for server, args in pairs(servers) do
+    local config = args or {}
+    config.capabilities = (args.capabilities or capabilities)
+    config.single_file_support = (args.single_file_support or true)
+    lspconfig[server].setup(config)
+  end
+
+  -- lspconfig.phpactor.setup({
+  --   autostart = true,
+  --   on_attach = function(client, bufnr)
+  --     client.server_capabilities.hoverProvider = false
+  --     client.server_capabilities.definitionProvider = false
+  --   end,
+  --   init_options = {
+  --     ["language_server_worse_reflection.inlay_hints.enable"] = true,
+  --     ["language_server_worse_reflection.inlay_hints.params"] = true,
+  --     ["language_server_worse_reflection.inlay_hints.types"] = false,
+  --     -- ["language_server_configuration.auto_config"] = false,
+  --     -- ["code_transform.import_globals"] = true,
+  --     -- ["language_server_phpstan.enabled"] = true,
+  --     -- ["language_server_phpstan.level"] = 7,
+  --     -- ["language_server_phpstan.bin"] = "phpstan",
+  --   },
+  -- })
+
+  require('lspconfig').yamlls.setup {
+    settings = {
+      yaml = {
+        schemas = {
+          -- kubernetes = "/*.k8s.{yml,yaml}",
+          ["https://gitlab.com/gitlab-org/gitlab/-/raw/master/app/assets/javascripts/editor/schema/ci.json"] = "*gitlab-ci*.{yml,yaml}",
+          ["https://raw.githubusercontent.com/yannh/kubernetes-json-schema/master/master/all.json"] = "/*.k8s.{yaml,yml}",
+        },
+      },
+    }
+  }
+  vim.lsp.inlay_hint.enable(true)
 end
 
--- Check if lsp-options-custom.lua exists
-local user_lsp_config_path = vim.fn.stdpath('config')..'/lua/lsp-options-custom.lua'
-local lsp_options
-
-if vim.fn.filereadable(user_lsp_config_path) == 1 then
-  lsp_options = require'lsp-options-custom'
-else
-  lsp_options = require'lsp-options'
-end
-local servers = lsp_options.servers
-
-for server, args in pairs(servers) do
-  local config = args or {}
-  config.capabilities = (args.capabilities or capabilities)
-  config.single_file_support = (args.single_file_support or true)
-  lspconfig[server].setup(config)
-end
-
--- lspconfig.phpactor.setup({
---   autostart = true,
---   on_attach = function(client, bufnr)
---     client.server_capabilities.hoverProvider = false
---     client.server_capabilities.definitionProvider = false
---   end,
---   init_options = {
---     ["language_server_worse_reflection.inlay_hints.enable"] = true,
---     ["language_server_worse_reflection.inlay_hints.params"] = true,
---     ["language_server_worse_reflection.inlay_hints.types"] = false,
---     -- ["language_server_configuration.auto_config"] = false,
---     -- ["code_transform.import_globals"] = true,
---     -- ["language_server_phpstan.enabled"] = true,
---     -- ["language_server_phpstan.level"] = 7,
---     -- ["language_server_phpstan.bin"] = "phpstan",
---   },
--- })
-
-require('lspconfig').yamlls.setup {
-  settings = {
-    yaml = {
-      schemas = {
-        -- kubernetes = "/*.k8s.{yml,yaml}",
-        ["https://gitlab.com/gitlab-org/gitlab/-/raw/master/app/assets/javascripts/editor/schema/ci.json"] = "*gitlab-ci*.{yml,yaml}",
-        ["https://raw.githubusercontent.com/yannh/kubernetes-json-schema/master/master/all.json"] = "/*.k8s.{yaml,yml}",
+return {
+  "neovim/nvim-lspconfig",
+  event = { "BufReadPost", "BufNewFile" },
+  cmd = { "Mason", "LspInstall", "LspUninstall", "LspInfo" },
+  config = configuration,
+  dependencies = {
+    {
+      "williamboman/mason-lspconfig.nvim",
+      config = true,
+      opts = { automatic_installation = true },
+      dependencies = {
+        "williamboman/mason.nvim",
+        config = true,
       },
     },
-  }
+  },
 }
-vim.lsp.inlay_hint.enable(true)
