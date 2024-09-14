6696a2385baaa9ff7cbf3bb43a7951902d064948
Mirek Nguyen <mirek.nguyen@o2.cz>
2024-09-14 14:54:41 +0200

    chore: move plugins

---
+++ lua/plugins/kitty-scrollback.lua
@@ -0,0 +1,6 @@
+return {
+  "mikesmithgh/kitty-scrollback.nvim",
+  config = true,
+  cmd = { "KittyScrollbackGenerateKittens", "KittyScrollbackCheckHealth" },
+  event = { "User KittyScrollbackLaunch" },
+}
---
+++ lua/plugins/lsp-signature.lua
@@ -0,0 +1,8 @@
+return {
+  "ray-x/lsp_signature.nvim",
+  event = "InsertEnter",
+  config = true,
+  opts = {
+    hint_enable = false,
+  },
+}
