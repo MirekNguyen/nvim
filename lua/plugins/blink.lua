74ac2810a40156572d0016354ced6f458df78161
Mirek Nguyen <mirek.nguyen@o2.cz>
2024-11-02 12:07:02 +0100

    feat(blink): update nerd font variant

--- lua/plugins/blink.lua
+++ lua/plugins/blink.lua
@@ -33,7 +33,7 @@ return {
     highlight = {
       use_nvim_cmp_as_default = true,
     },
-    nerd_font_variant = "normal",
+    nerd_font_variant = "mono",
     accept = { auto_brackets = { enabled = true } },
     -- trigger = {
     --   signature_help = {
