81f49516b0b61ecb766bdfeeb485a37dd6f42d24
Mirek Nguyen <mirek.nguyen@o2.cz>
2024-11-14 14:40:50 +0100

    feat: toggleterm

---
+++ lua/plugins/toggleterm.lua
@@ -0,0 +1,8 @@
+return {
+  "akinsho/toggleterm.nvim",
+  version = "*",
+  keys = {
+    { "<leader>lt", "<cmd>ToggleTerm direction=float<CR>",                                          desc = "ToggleTerm" },
+  },
+  opts = {},
+}
