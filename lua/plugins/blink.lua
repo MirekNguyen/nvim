9a1645e1f1c56401b684922b5cdd65b6ee1ca0c7
Mirek Nguyen <mirek.nguyen@o2.cz>
2024-11-14 14:42:20 +0100

    feat: blink support for dadbod

--- lua/plugins/blink.lua
+++ lua/plugins/blink.lua
@@ -1,35 +1,44 @@
 return {
-	"saghen/blink.cmp",
-	enabled = true,
-	event = { "InsertCharPre" },
-	version = "v0.*",
-  dependencies = 'rafamadriz/friendly-snippets',
-	opts = {
-		keymap = {
-			["<C-a>"] = { "show", "show_documentation", "hide_documentation" },
-			["<C-e>"] = { "hide", "fallback" },
-			["<CR>"] = { "accept", "fallback" },
+  "saghen/blink.cmp",
+  enabled = true,
+  event = { "InsertCharPre" },
+  version = "v0.*",
+  dependencies = "rafamadriz/friendly-snippets",
+  opts = {
+    keymap = {
+      ["<C-a>"] = { "show", "show_documentation", "hide_documentation" },
+      ["<C-e>"] = { "hide", "fallback" },
+      ["<CR>"] = { "accept", "fallback" },
 
-      ['<Tab>'] = {
+      ["<Tab>"] = {
         function(cmp)
-          if cmp.is_in_snippet() then return cmp.accept()
+          if cmp.is_in_snippet() then
+            return cmp.accept()
           end
         end,
-        'select_next',
-        'snippet_forward',
-        'fallback'
+        "select_next",
+        "snippet_forward",
+        "fallback",
       },
-      ['<S-Tab>'] = { 'select_prev', 'snippet_backward', 'fallback' },
-			["<C-p>"] = { "select_prev", "fallback" },
-			["<C-n>"] = { "select_next", "fallback" },
-			["<Up>"] = { "select_prev", "fallback" },
-			["<Down>"] = { "select_next", "fallback" },
+      ["<S-Tab>"] = { "select_prev", "snippet_backward", "fallback" },
+      ["<C-p>"] = { "select_prev", "fallback" },
+      ["<C-n>"] = { "select_next", "fallback" },
+      ["<Up>"] = { "select_prev", "fallback" },
+      ["<Down>"] = { "select_next", "fallback" },
 
-			["<C-b>"] = { "scroll_documentation_up", "fallback" },
-			["<C-f>"] = { "scroll_documentation_down", "fallback" },
-		},
-		autocomplete = {
-		},
+      ["<C-b>"] = { "scroll_documentation_up", "fallback" },
+      ["<C-f>"] = { "scroll_documentation_down", "fallback" },
+    },
+    autocomplete = {},
+    sources = {
+      -- add vim-dadbod-completion to your completion providers
+      completion = {
+        enabled_providers = { "lsp", "path", "snippets", "buffer", "dadbod" },
+      },
+      providers = {
+        dadbod = { name = "Dadbod", module = "vim_dadbod_completion.blink" },
+      },
+    },
     highlight = {
       use_nvim_cmp_as_default = true,
     },
@@ -41,20 +50,20 @@ return {
     --     show_on_insert_on_trigger_character = true,
     --   },
     -- },
-		windows = {
-			autocomplete = {
-			  draw = 'reversed', -- 'minimal' | 'reversed' | 'simple'
-				border = "single",
-			},
-			documentation = {
-				auto_show = true,
+    windows = {
+      autocomplete = {
+        draw = "reversed", -- 'minimal' | 'reversed' | 'simple'
+        border = "single",
+      },
+      documentation = {
+        auto_show = true,
         auto_show_delay_ms = 250,
         -- update_delay_ms = 0,
-				border = "single",
-			},
+        border = "single",
+      },
       -- ghost_text = {
       --   enabled = true,
       -- },
-		},
-	},
+    },
+  },
 }
