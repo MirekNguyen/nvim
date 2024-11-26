245196c470981b316a82d40dea87b95040ae7935
Mirek Nguyen <mirek.nguyen@o2.cz>
2024-11-26 18:23:51 +0100

    feat: linux servers configuration

--- lua/plugins/kitty-scrollback.lua
+++
@@ -1,6 +0,0 @@
-return {
-  "mikesmithgh/kitty-scrollback.nvim",
-  opts = {},
-  cmd = { "KittyScrollbackGenerateKittens", "KittyScrollbackCheckHealth" },
-  event = { "User KittyScrollbackLaunch" },
-}
--- lua/plugins/toggleterm.lua
+++
@@ -1,8 +0,0 @@
-return {
-  "akinsho/toggleterm.nvim",
-  version = "*",
-  keys = {
-    { "<leader>lt", "<cmd>ToggleTerm direction=float<CR>",                                          desc = "ToggleTerm" },
-  },
-  opts = {},
-}
