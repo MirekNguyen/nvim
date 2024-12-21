4790d960887f31f583329dc5875e7c98e5ae15dc
Mirek Nguyen <mirek.nguyen@o2.cz>
2024-12-22 00:42:59 +0100

    feat: ansible filetype detection

--- filetype.lua
+++ filetype.lua
@@ -3,3 +3,24 @@ vim.filetype.add({
     http = "http",
   },
 })
+
+vim.filetype.add({
+  pattern = {
+    ["Chart%.ya?ml"] = "helm",
+    [".*/templates/.*%.ya?ml"] = "helm",
+    [".*/values.*%.ya?ml"] = "helm",
+  },
+})
+
+vim.filetype.add({
+  pattern = {
+    [".*playbook.*%.ya?ml"] = "yaml.ansible",
+    [".*/roles/.*/tasks/.*%.ya?ml"] = "yaml.ansible",
+    [".*/roles/.*/handlers/.*%.ya?ml"] = "yaml.ansible",
+    [".*/group_vars/.*%.ya?ml"] = "yaml.ansible",
+    [".*/host_vars/.*%.ya?ml"] = "yaml.ansible",
+    [".*/inventory/.*%.ini"] = "yaml.ansible",
+    [".*/site%.ya?ml"] = "yaml.ansible",
+    [".*/inventory%.ya?ml"] = "yaml.ansible",
+  },
+})
