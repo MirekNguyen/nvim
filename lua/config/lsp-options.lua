ae45ff0500982f30e2a9a77bf868d324d0288cd3
Mirek Nguyen <mirek.nguyen@o2.cz>
2024-12-23 23:29:02 +0100

    feat: yamlls kubernetes for yml

--- lua/config/lsp-options.lua
+++ lua/config/lsp-options.lua
@@ -41,12 +41,11 @@ return {
 			settings = {
 				yaml = {
 					schemas = {
-						 kubernetes = "*.yaml",
+						 kubernetes = "*.{yml,yaml}",
 						["https://gitlab.com/gitlab-org/gitlab/-/raw/master/app/assets/javascripts/editor/schema/ci.json"] = "*gitlab-ci*.{yml,yaml}",
 						["https://raw.githubusercontent.com/yannh/kubernetes-json-schema/master/master/all.json"] = "/*.k8s.{yaml,yml}",
 						["https://json.schemastore.org/github-workflow.json"] = ".github/workflows/*.{yml,yaml}",
 						["http://json.schemastore.org/github-action"] = ".github/action.{yml,yaml}",
-						["https://raw.githubusercontent.com/argoproj/argo-workflows/master/api/jsonschema/schema.json"] = "apps/*.{yml,yaml}",
 					},
 				},
 			},
