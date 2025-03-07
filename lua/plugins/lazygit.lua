9dd626406b0f7ff9ff91689c9fcc4d12600fdada
Mirek Nguyen <mirek.nguyen@o2.cz>
2025-03-07 17:39:23 +0100

    feat(plugin): add nvim-ts-context-commentstring for better comment handling

--- lua/plugins/lazygit.lua
+++ lua/plugins/lazygit.lua
@@ -11,6 +11,6 @@ return {
         "nvim-lua/plenary.nvim",
     },
     keys = {
-        { "<leader>lg", "<cmd>LazyGit<cr>", desc = "LazyGit" }
+        { "<leader>g", "<cmd>LazyGit<cr>", desc = "LazyGit" }
     }
 }
