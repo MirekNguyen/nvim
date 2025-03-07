2faf5ae9e09a2030f1e62a026124dc2e2d7dace3
Mirek Nguyen <mirek.nguyen@o2.cz>
2025-03-07 01:34:12 +0100

    feat(plugin): integrate tailwind-tools for enhanced Tailwind CSS support

---
+++ lua/plugins/tailwind-tools.lua
@@ -0,0 +1,12 @@
+return {
+	"luckasRanarison/tailwind-tools.nvim",
+	name = "tailwind-tools",
+	build = ":UpdateRemotePlugins",
+	ft = {
+		"javascript",
+		"javascriptreact",
+		"typescript",
+		"typescriptreact",
+	},
+	opts = {},
+}
