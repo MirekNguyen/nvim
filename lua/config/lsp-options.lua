1c12d6c50f4799db4065e63d43452c6560e64a90
Mirek Nguyen <mirek.nguyen@o2.cz>
2025-03-21 17:13:18 +0100

    style: Update file pattern for Kubernetes schema

--- lua/config/lsp-options.lua
+++ lua/config/lsp-options.lua
@@ -41,7 +41,7 @@ return {
 			settings = {
 				yaml = {
 					schemas = {
-						kubernetes = "*.{yml,yaml}",
+						kubernetes = "*.k8s.{yml,yaml}",
 						["https://gitlab.com/gitlab-org/gitlab/-/raw/master/app/assets/javascripts/editor/schema/ci.json"] = "*gitlab-ci*.{yml,yaml}",
 						["https://raw.githubusercontent.com/yannh/kubernetes-json-schema/master/master/all.json"] = "/*.k8s.{yaml,yml}",
 						["https://json.schemastore.org/github-workflow.json"] = ".github/workflows/*.{yml,yaml}",
