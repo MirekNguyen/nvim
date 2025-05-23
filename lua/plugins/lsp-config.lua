dcc554767c439ae24929da5e78ed224654ea72d3
Mirek Nguyen <mirek.nguyen@o2.cz>
2025-05-23 11:31:01 +0200

    feat: modernize config

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -1,22 +1,21 @@
-local function configuration()
-	local capabilities = vim.lsp.protocol.make_client_capabilities()
-	capabilities.textDocument.completion.completionItem.snippetSupport = true
-
-	local signs = { Error = "", Warn = "", Hint = "", Info = "" }
-	for type, icon in pairs(signs) do
-		local hl = "DiagnosticSign" .. type
-		vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
-	end
-
-	vim.lsp.inlay_hint.enable(true)
-end
+vim.diagnostic.config({
+	signs = {
+		text = {
+			[vim.diagnostic.severity.ERROR] = "",
+			[vim.diagnostic.severity.WARN] = "",
+			[vim.diagnostic.severity.HINT] = "",
+			[vim.diagnostic.severity.INFO] = "",
+		},
+	},
+})
+vim.lsp.inlay_hint.enable(true)
+local capabilities = vim.lsp.protocol.make_client_capabilities()
+capabilities.textDocument.completion.completionItem.snippetSupport = true
 
 return {
 	"neovim/nvim-lspconfig",
 	event = { "BufReadPost", "BufNewFile" },
-	version = "*",
 	cmd = { "Mason", "LspInstall", "LspUninstall", "LspInfo" },
-	config = configuration,
 	dependencies = {
 		{
 			"williamboman/mason.nvim",
--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,23 +1,23 @@
 local tsconfig = {
-  ensure_installed = "all",
-  highlight = {
-    enable = true,
-    disable = {},
-  },
-  indent = {
-    enable = true,
-    disable = {},
-  },
-  autotag = {
-    enable = false,
-  },
+	ensure_installed = "all",
+	highlight = {
+		enable = true,
+		disable = {},
+	},
+	indent = {
+		enable = true,
+		disable = {},
+	},
+	autotag = {
+		enable = false,
+	},
 }
 
 return {
-  "nvim-treesitter/nvim-treesitter",
-  event = { "BufReadPost", "BufNewFile" },
-  build = ":TSUpdate",
-  config = function()
-    require("nvim-treesitter.configs").setup(tsconfig)
-  end,
+	"nvim-treesitter/nvim-treesitter",
+	event = { "VeryLazy" },
+	build = ":TSUpdate",
+	config = function()
+		require("nvim-treesitter.configs").setup(tsconfig)
+	end,
 }
