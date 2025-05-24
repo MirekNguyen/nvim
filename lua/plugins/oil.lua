469fb5620b1734268da96bd18c646d115570e578
Mirek Nguyen <mirek.nguyen@o2.cz>
2025-05-24 17:13:51 +0200

    feat: lsp autosave file operations

--- lua/plugins/oil.lua
+++ lua/plugins/oil.lua
@@ -2,11 +2,14 @@ return {
 	"stevearc/oil.nvim",
 	cmd = "Oil",
 	keys = {
-		{ "<leader>k", "<cmd>Oil<cr>", desc ="Open Oil" },
+		{ "<leader>k", "<cmd>Oil<cr>", desc = "Open Oil" },
 	},
 	opts = {
 		view_options = {
 			show_hidden = true,
+		},
+		lsp_file_methods = {
+			autosave_changes = true,
 		},
 	},
 }
