b558febdcbf3cb745cd75f4e77857d62ac3e5bea
Mirek Nguyen <mirek.nguyen@o2.cz>
2025-05-28 21:26:49 +0200

    feat: remove rest nvim

--- lua/plugins/kulala.lua
+++ lua/plugins/kulala.lua
@@ -1,12 +1,15 @@
 return {
-  "mistweaverco/kulala.nvim",
-  keys = {
-    { "<leader>Rs", desc = "Send request" },
-    { "<leader>Ra", desc = "Send all requests" },
-    { "<leader>Rb", desc = "Open scratchpad" },
-  },
-  ft = { "http", "rest" },
-  opts = {
-    global_keymaps = true,
-  },
+	"mistweaverco/kulala.nvim",
+	keys = {
+		{ "<leader>Rs", desc = "Send request" },
+		{ "<leader>Ra", desc = "Send all requests" },
+		{ "<leader>Rb", desc = "Open scratchpad" },
+	},
+	ft = { "http", "rest" },
+	opts = {
+		global_keymaps = true,
+		ui = {
+			formatter = false,
+		},
+	},
 }
