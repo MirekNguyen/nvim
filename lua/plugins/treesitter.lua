d129f5cf88845dd79ccebba7df20a49407295abf
Mirek Nguyen <mirek.nguyen@o2.cz>
2026-04-26 15:56:13 +0200

    refactor(treesitter): use native start via autocmd

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,60 +1,27 @@
-local tsconfig = {
-	ensure_installed = {
-    "bash",
-    "c",
-    "c_sharp",
-    "cpp",
-    "css",
-    "fish",
-    "html",
-    "java",
-    "javascript",
-    "json",
-    "lua",
-    "php",
-    "phpdoc",
-    "python",
-    "scss",
-    "tsx",
-    "typescript",
-    "vim",
-    "yaml",
-    "markdown",
-    "markdown_inline",
-    "twig",
-    "sql",
-    "xml",
-    "rust",
-    "gitignore",
-    "dockerfile",
-    "svelte",
-    "toml",
-    "requirements",
-    "prisma",
-    "http",
-    "graphql",
-    "go",
-    "nix",
-    "swift",
-  },
-	highlight = {
-		enable = true,
-		disable = {},
-	},
-	indent = {
-		enable = true,
-		disable = {},
-	},
-	autotag = {
-		enable = false,
-	},
+local parsers_to_install = {
+  "bash", "c", "c_sharp", "cpp", "css", "fish", "html", "java",
+  "javascript", "json", "lua", "php", "phpdoc", "python", "scss",
+  "tsx", "typescript", "vim", "yaml", "markdown", "markdown_inline",
+  "twig", "sql", "xml", "rust", "gitignore", "dockerfile", "svelte",
+  "toml", "requirements", "prisma", "http", "graphql", "go", "nix", "swift",
 }
 
 return {
-	"nvim-treesitter/nvim-treesitter",
-	event = { "VeryLazy" },
-	build = ":TSUpdate",
-	config = function()
-		require("nvim-treesitter.configs").setup(tsconfig)
-	end,
+  "nvim-treesitter/nvim-treesitter",
+  branch = "main",
+  event = { "VeryLazy" },
+  build = ":TSUpdate",
+  config = function()
+    local nvim_treesitter = require("nvim-treesitter")
+
+    nvim_treesitter.install(parsers_to_install)
+
+    vim.api.nvim_create_autocmd("FileType", {
+      group = vim.api.nvim_create_augroup("treesitter_setup", { clear = true }),
+      callback = function(event)
+        pcall(vim.treesitter.start, event.buf)
+        vim.bo[event.buf].indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+      end,
+    })
+  end,
 }
